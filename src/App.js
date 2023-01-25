825c8c2067854ea9dece828099d41e91792bc121
Vladimir RM <shvdrummer@gmail.com>
2023-01-25 18:11:50 +0200

    add content

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 
 const App = () => {
+  const [showMode, setShowMode] = useState(true);
 
-  const [showMode,setShowMode]=useState(true)
+  const handleClick = () => {
+    setShowMode(!showMode);
+  };
+  let content = null;
 
-  
-  return (
-    <div>
-      
-    </div>
-  )
-}
 
-export default App
+  if(showMode){
+    content = <>
+    <h1>Hello world</h1>
+    <div>Message</div>
+    
+    </>
+  }
+  return <div></div>;
+};
+
+export default App;
