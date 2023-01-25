527f6e810a1e9030c3b252073449ce67bac1173c
Vladimir RM <shvdrummer@gmail.com>
2023-01-25 18:18:01 +0200

    check all work

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import './App.css'
 
 const App = () => {
   const [showMode, setShowMode] = useState(true);
@@ -19,7 +20,7 @@ const App = () => {
   return <div>
 
     <button onClick={handleClick}>Show toggle</button>
-    {showMode} </div>;
+    {content} </div>;
 };
 
 export default App;
